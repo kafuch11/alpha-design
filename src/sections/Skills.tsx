957a61ec0b862dbb1d777619f2790222f7b10d4f
kafuch11 <iamkafeelahmedch@gmail.com>
2026-02-25 07:24:37 -0800

    skill cards reframed

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -3,12 +3,12 @@ import SkillCard from "../components/cards/SkillCard"
 const Skills = () => {
   return (
     <div id="skills" className="section-cont">
+      <h2 className="text-2xl font-bold mb-4 ml-40">Skills</h2>
         <div className="section-card flex flex-wrap justify-center  ">
-            <SkillCard />
-            {/* <SkillCard />
-            <SkillCard />
-            <SkillCard />
-            <SkillCard /> */}
+          {["JavaScript", "React", "Node.js", "TypeScript", "CSS"].map((skill) => (
+            <SkillCard key={skill} text={skill} />
+          ))}
+            
         </div>
     </div>
   )
