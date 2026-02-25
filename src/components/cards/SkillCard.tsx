9a897157abc0f131dde84968480c2f39839e6532
kafuch11 <iamkafeelahmedch@gmail.com>
2026-02-25 07:15:22 -0800

    skill cards reframed

--- src/components/cards/SkillCard.tsx
+++ src/components/cards/SkillCard.tsx
@@ -1,18 +1,14 @@
 
-const SkillCard = () => {
+
+const SkillCard = ({text}:{text:string}) => {
   return (
     <div className="skills-card">
-      <div className=" h-36 aspect-square border-8 border-green-500 rounded-full flex flex-col gap-0 justify-center items-center">
-        <div className="text-3xl font-bold text-green-600">98%</div>
+      <div className="w-8 h-8 rounded-full border-2 border-r-gray-500">
+
       </div>
-      <div className="text-lg line-1 text-green-600 ">Expert</div>
-        <div className="flex gap-1 items-center">
-          <div className="h-5 aspect-square rounded-full border border-gray-700"></div>
-          <div className="text-xl text-gray-600">Reactjs</div>
-        </div>
+      <div className="text-lg font-bold">{text}</div>
     </div>
   )
 }
 
 export default SkillCard;
-
